268cdeed4828f1c54412c60c7eef92dee95493cb
Aleksandr Drogalin <drogalin.alexander@yandex.ru>
2025-08-18 23:21:34 +0300

    the first task is completed

---
+++ src/main/java/EvenNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+        System.out.println("Введите число");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Число четное!");
+        } else {
+            System.out.println("Число нечетное!");
+        }
+
+        scanner.close();
+    }
+}
